223ffa94a85c79d0729d9730d64ec1ea14e42061
Andrew N. Smith <asmith@owlshares.com>
2025-01-11 02:16:30 -0500

    scaffold api methods for core lifecycle routes

--- form-vmm/vmm-service/src/api/mod.rs
+++ form-vmm/vmm-service/src/api/mod.rs
@@ -20,18 +20,26 @@ pub struct VmmApiChannel {
 }
 
 impl VmmApiChannel {
-    pub fn new(tx: mpsc::Sender<VmmEvent>, rx: mpsc::Receiver<String>) -> Self {
+    pub fn new(
+        tx: mpsc::Sender<VmmEvent>,
+        rx: mpsc::Receiver<String>
+    ) -> Self {
         Self{
             event_sender: tx,
             response_receiver: rx
         }
     }
 
-    pub async fn send(&self, event: VmmEvent) -> Result<(), mpsc::error::SendError<VmmEvent>> {
+    pub async fn send(
+        &self,
+        event: VmmEvent
+    ) -> Result<(), mpsc::error::SendError<VmmEvent>> {
         self.event_sender.send(event).await
     }
 
-    pub async fn recv<T: DeserializeOwned>(&mut self) -> Option<T> {
+    pub async fn recv<T: DeserializeOwned>(
+        &mut self
+    ) -> Option<T> {
         match self.response_receiver.recv().await {
             Some(value) => {
                 match serde_json::from_str::<T>(&value) {
@@ -70,6 +78,30 @@ pub struct CreateVmRequest {
     pub name: String,
     pub user_data: Option<String>,
     pub meta_data: Option<String>,
+    pub signature: Option<String>,
+    pub recovery_id: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StartVmRequest {
+    pub id: String,
+    pub name: String,
+    pub signature: Option<String>,
+    pub recovery_id: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct StopVmRequest {
+    pub id: String,
+    pub name: String,
+    pub signature: Option<String>,
+    pub recovery_id: u32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct DeleteVmRequest {
+    pub id: String,
+    pub name: String,
     pub signature: Option<String>,
     pub recovery_id: u32,
 }
@@ -134,8 +166,17 @@ impl VmmApi {
             .with_state(app_state);
 
         log::info!("Established route, binding to {}", &self.addr);
-        let listener = tokio::net::TcpListener::bind(self.addr.clone()).await
-            .map_err(|e| VmmError::SystemError(format!("Failed to bind listener to address {}: {e}", self.addr.clone())))?;
+        let listener = tokio::net::TcpListener::bind(
+            self.addr.clone()).await
+            .map_err(|e| {
+                VmmError::SystemError(
+                    format!(
+                        "Failed to bind listener to address {}: {e}",
+                        self.addr.clone()
+                    )
+                )
+            })?;
+            
         // Start the API server
         log::info!("Starting server");
         axum::serve(listener, app).await
@@ -154,6 +195,14 @@ async fn health_check() -> &'static str {
     "OK"
 }
 
+async fn ping(
+    State(channel): State<Arc<Mutex<VmmApiChannel>>>,
+    Json(request): Json<PingVmmRequest>
+) -> Result<Json<VmmPingResponse>, String> {
+    let event = VmmEvent::Ping { name: request.name.to_string() };
+    request_receive(channel, event).await
+}
+
 async fn create(
     State(channel): State<Arc<Mutex<VmmApiChannel>>>,
     Json(request): Json<CreateVmRequest>,
@@ -163,6 +212,9 @@ async fn create(
         request.name, request.distro, request.version
     );
     // Convert request into a VmmEvent::Create
+    // TODO: Recover owner from signature
+    // TODO: Check against balance 
+    // TODO: Generate ID
     let event = VmmEvent::Create {
         owner: "test".to_string(),
         recovery_id: 0,
@@ -179,40 +231,48 @@ async fn create(
         console_type: None,
     };
 
-    log::info!("Sending create event to VMM service: {:?}", event);
-
-    channel.lock().await.send(event).await.map_err(|e| e.to_string())?;
-
-    log::info!("VM Creation request processed for {}", request.name);
+    let _ = request_receive::<()>(channel, event).await?;
 
     Ok(Json(VmResponse {
         id: "pending".to_string(),
         name: request.name,
-        state: "creating".to_string()
+        state: "pending".to_string()
     }))
 
 }
 
-async fn ping(
+async fn start(
     State(channel): State<Arc<Mutex<VmmApiChannel>>>,
-    Json(request): Json<PingVmmRequest>
-) -> Result<Json<VmmPingResponse>, String> {
-    let event = VmmEvent::Ping { name: request.name.to_string() };
-    let mut channel = channel.lock().await; 
-    channel.send(event).await.map_err(|e| e.to_string())?;
-    tokio::select! {
-        Some(resp) = channel.recv() => {
-            Ok(Json(resp))
-        }
-        _ = tokio::time::sleep(Duration::from_secs(5)) => {
-            Err("Ping request timed out awaiting response".to_string())
-        }
-    }
+    Json(request): Json<StartVmRequest>,
+) -> Result<Json<VmResponse>, String> {
+    let event = VmmEvent::Start {
+        id: request.id.clone(),
+        owner: "test".to_string(),
+        recovery_id: 0,
+        requestor: "node".to_string(),
+    };
+    let _ = request_receive::<()>(channel, event).await?;
+    Ok(Json(VmResponse {
+        id: request.id, 
+        name: request.name,
+        state: "pending".to_string()
+    }))
 }
 
-async fn start() {}
-async fn stop() {}
-async fn delete() {}
+async fn stop(
+    State(channel): State<Arc<Mutex<VmmApiChannel>>>,
+    Json(request): Json<StopVmRequest>,
+) -> Result<Json<VmResponse>, String> {
+    todo!()
+}
+
+async fn delete(
+    State(channel): State<Arc<Mutex<VmmApiChannel>>>,
+    Json(request): Json<DeleteVmRequest>,
+) -> Result<Json<VmResponse>, String> {
+    todo!()
+}
+
 async fn get_vm() {}
 async fn list() {}
 async fn power_button() {}
@@ -229,3 +289,19 @@ async fn add_fs() {}
 async fn remove_device() {}
 async fn migrate_to() {}
 async fn migrate_from() {}
+
+async fn request_receive<T: DeserializeOwned>(
+    channel: Arc<Mutex<VmmApiChannel>>,
+    event: VmmEvent,
+) -> Result<Json<T>, String> {
+    let mut channel = channel.lock().await; 
+    channel.send(event).await.map_err(|e| e.to_string())?;
+    tokio::select! {
+        Some(resp) = channel.recv() => {
+            Ok(Json(resp))
+        }
+        _ = tokio::time::sleep(Duration::from_secs(5)) => {
+            Err("Ping request timed out awaiting response".to_string())
+        }
+    }
+}
