72641629bf1577b8f7158ee31e2183b81aebc325
Andrew N. Smith <asmith@owlshares.com>
2025-01-22 02:02:29 -0500

    build datastore for network, node and instance state, implement network state so that it can be integrated with formnet, and replace the SQLite and client-server model, moving to a P2P/Decentralized model. Need to add test coverage and need to integrate network state with formnet

--- form-p2p/src/lib.rs
+++ form-p2p/src/lib.rs
@@ -1,13 +1,1 @@
-pub mod formation_rpc {
-    tonic::include_proto!("formation_rpc");
-    pub const FILE_DESCRIPTOR_SET: &[u8] =
-        tonic::include_file_descriptor_set!("formation_descriptor");
-}
-
-pub mod heartbeat;
-pub mod join;
 pub mod server;
-pub mod client;
-pub mod helpers;
-
-pub use helpers::*;
