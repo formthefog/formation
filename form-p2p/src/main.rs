b2e678933d3aa373306f5b623136eba08b2cf28d
Andrew N. Smith <asmith@owlshares.com>
2025-02-05 16:23:46 -0500

    scaffold queue main

--- form-p2p/src/main.rs
+++ form-p2p/src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+use alloy::{primitives::Address, signers::k256::ecdsa::SigningKey};
+use clap::{Parser, Subcommand};
+use form_p2p::queue::{FormMQ, QUEUE_PORT};
+use tokio::sync::RwLock;
+use std::{path::PathBuf, sync::Arc};
+use form_config::OperatorConfig;
+
+#[derive(Parser, Debug)]
+#[command(name = "form-mq", about = "Formation Message Queue")]
+pub struct CliArgs {
+    /// Enable debug logging
+    #[arg(short, long, default_value="false")]
+    pub debug: bool,
+    #[arg(short='C', long, default_value_os_t=PathBuf::from("/etc/formation/.operator-config.json"))]
+    pub config: PathBuf,
+    #[arg(short, long, default_value="true")]
+    pub encrypted: bool,
+    #[arg(short, long)]
+    pub password: Option<String>,
+    /// Command to execute
+    #[command(subcommand)]
+    pub command: CliCommand,
+}
+
+#[derive(Subcommand, Debug)]
+pub enum CliCommand {
+    /// Run the VMM service
+    #[command(name = "run")]
+    Run {
+        #[clap(aliases=["secret-key", "private-key"])]
+        signing_key: Option<String>,
+        /// Message broker subscriber address
+        #[arg(long, short)]
+        sub_addr: Option<String>,
+        /// Message broker Publish Address
+        #[arg(long, short)]
+        pub_addr: Option<String>,
+    },
+    /// Show service status
+    #[command(name = "status")]
+    Status,
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Setup the logger
+    simple_logger::init_with_level(log::Level::Info)
+        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))?;
+
+    let args = CliArgs::parse();
+    let config = OperatorConfig::from_file(args.config, args.encrypted, args.password.as_deref()).ok();
+
+    // Parse command line args
+    let args = CliArgs::parse();
+    let config = OperatorConfig::from_file(args.config, args.encrypted, args.password.as_deref()).ok();
+    match args.command {
+        CliCommand::Run { signing_key, sub_addr, pub_addr } => {
+            let signing_key = if signing_key.is_none() {
+                let config = config.unwrap();
+                config.secret_key.unwrap()
+            } else {
+                signing_key.unwrap()
+            };
+            let address = hex::encode(
+                Address::from_private_key(
+                    &SigningKey::from_slice(
+                        &hex::decode(&signing_key)?
+                    )?
+                )
+            );
+            let queue = Arc::new(RwLock::new(FormMQ::new(address, signing_key, String::new())));
+            let (shutdown_tx, shutdown_rx) = tokio::sync::broadcast::channel(1024);
+            let inner_queue = queue.clone();
+            let handle = tokio::spawn(async move {
+                if let Err(e) = form_p2p::api::serve(inner_queue, QUEUE_PORT).await {
+                    eprintln!("Error serving queue api: {e}");
+                } 
+            });
+            let _ = tokio::signal::ctrl_c().await;
+            shutdown_tx.send(())?;
+            handle.await?;
+        }
+        _ => {}
+    }
+
+    Ok(())
 }
